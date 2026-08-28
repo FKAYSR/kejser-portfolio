45119f04fc1f981352c6f9731300c4271f7411c5
FKAYSR <kejserfreja@gmail.com>
2026-08-28 20:19:59 +0200

    Folders and files made for pages and components. App.jsx file Routing setup to the newly made pages and relevant components. global.css file made for global variables and styling purposes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { Routes, Route } from "react-router";
+import Home from "../src/pages/Home.jsx";
+import About from "../src/pages/About.jsx";
+import Contact from "../src/pages/Contact.jsx";
+import Overview from "../src/pages/Overview.jsx";
+import ProjectDetail from "../src/pages/ProjectDetail.jsx";
+import NotFound from "../src/pages/NotFound.jsx";
+import Navbar from "../src/components/Navbar.jsx";
+import Footer from "../src/components/Footer.jsx";
 
 function App() {
-  return (
-    <div>
-      <h1>Freja Kejser</h1>
-    </div>
-  )
+    return (
+      <>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/project/:slug" element={<ProjectDetail />} />
+        </Routes>
+
+        <Footer />
+      </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/Overview.jsx
@@ -0,0 +1,7 @@
+export default function Overview() {
+  return (
+    <div>
+      <h1>overview of projects</h1>
+    </div>
+  );
+}
