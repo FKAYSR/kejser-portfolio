076bcf0426960abbe584463f1e3364bc26090a95
FKAYSR <kejserfreja@gmail.com>
2026-09-07 00:36:55 +0200

    Add initial projects data structure and template for projects data

---
+++ src/data/projects.js
@@ -0,0 +1,93 @@
+const projects = [
+];
+
+export default projects;
+
+/* 
+========================================
+NEW PROJECT TEMPLATE
+========================================
+{
+  id: "",
+  title: "",
+  thumbnail: "",
+  tags: [],
+
+  // Visibility
+  showOnHome: false,
+  homeOrder: 1,
+  showOnOverview: true,
+
+  // Short descriptions
+  homeDescription: "",
+  overviewDescription: "",
+
+  // Project detail page
+  detail: {
+    heroImage: "",
+    links: {
+    live: "",
+    repository: "",
+    designFile: "",
+    figmaPrototype: "",
+    },
+
+    overview: {
+      description: "",
+      description2: "",
+    },
+
+    assignment: {
+      type: "",
+      problem: "",
+      targetAudience: "",
+    },
+
+    solution: {
+      created: "",
+      keyFeatures: [],
+      valueCreation: [],
+    },
+
+    toolsUsed: []
+
+    myRole: [
+    {
+        title: "",
+        text: "",
+        bigImage: "",
+        image2: "",
+        image3: "",
+    },
+    {
+        title: "",
+        text: "",
+        bigImage: "",
+        image2: "",
+        image3: "",
+    },
+    {
+        title: "",
+        text: "",
+        bigImage: "",
+        image2: "",
+        image3: "",
+    },
+    ],
+
+    whatILearned: [
+    "",
+    "",
+    "",
+    ],
+
+    ifIDidItAgain: [
+    "",
+    "",
+    "",
+    ],
+
+    teamFeedback: "",
+  },
+},
+*/
